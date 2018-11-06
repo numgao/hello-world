2ea9b60da308133b505c04ab1471dfe9d7fc45e3
numgao <paul.gao@gmail.com>
2018-11-06 13:21:57 +0800

    aa

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ r = requests.get(url)
 r.status_code
 print(r.status_code)
 r.text
-print(r.text[:500])
-print("这个是测试")
-print("这是另一个测试")
-print("这个测试是在2018-11-08早上9：08开始的")
+    print(r.text[:500])
+    print("这个是测试")
+    print("这是另一个测试")
+    print("这个测试是在2018-11-08早上9：08开始的")
+
