d385b2f83eae9d2c3df3f76fb3868395df6d4a83
numgao <paul.gao@gmail.com>
2018-11-20 15:12:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-import requests
-url = 'https://www.baidu.com'
-r = requests.get(url)
-r.status_code
-print(r.status_code)
-r.text
-    print(r.text[:500])
-    print("这个是测试")
-    print("这是另一个测试")
-    print("这个测试是在2018-11-08早上9：08开始的")
+
+
+print("这个是测试")
+print("这是另一个测试")
+print("这个测试是在2018-11-08早上9：08开始的")
+print("hello,just")
+print("跳出大括号到行尾的命令式CTRL+}")
 
